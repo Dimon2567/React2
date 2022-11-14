1eba3d4e143e513d3797155dfca9023d72bab77a
Aleksey <ryabkoalex2008@gmail.com>
2022-11-14 19:20:30 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>Hello , world </h1>
+    </div>
+  );
+}
+export default App;
+export function Menu() {
+  return (
+    <div>
+      <a href="">link</a>
+      <a href="">link2</a>
+    </div>
+  );
+}
